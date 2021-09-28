008162059f1899f162d63e30380140072b15e406
baibrahimaparis <baibrahimaparis@gmail.com>
2021-09-28 16:51:45 +0200

    update

---
+++ guess-the-number-console/main.js
@@ -0,0 +1,25 @@
+console.log('helle user!')
+
+
+
+console.log ('try to guess the number')
+
+
+console.log ('Use " Submit(X) to number')
+const mysterynumber=45 
+
+const Submit=( x)=> { if ( x< mysterynumber)
+
+{console.log('too small')}
+if ( x> mysterynumber {console.log('too big')})
+
+if( x = = mysterynumber) { console.log( 'Hurrah!')}
+
+
+
+
+
+
+
+
+}
